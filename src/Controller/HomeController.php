0050c2f386cbd84a3d41ca684bd2a78a05f8fe81
JohnDoe <johndoe@example.com>
2020-08-18 11:20:25 +0200

    INSTALLE le packde debug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,9 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+
+        
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
